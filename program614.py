e3335a8b576eec5fe16ae6a790744297bdc60e01
Samyak Kailas Medhe <152603493+Samyakmedhe@users.noreply.github.com>
2026-01-28 21:48:25 +0530

    Add files via upload

---
+++ program614.py
@@ -0,0 +1,14 @@
+    
+def Display():
+
+    print("Jay Ganesh...")
+    print("Jay Ganesh...")
+    print("Jay Ganesh...")
+    print("Jay Ganesh...")
+    print("Jay Ganesh...")
+
+def main():
+    Display()
+    
+if __name__ =="__main__":
+    main()
---
+++ program615.py
@@ -0,0 +1,11 @@
+    
+def Display():
+   Arr = range(5)
+
+   print(Arr)
+
+def main():
+    Display()
+    
+if __name__ =="__main__":
+    main()
